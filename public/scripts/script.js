5b72a2ea4ab463fbef2948ad3fc0dffc3e092845
Jesper <jesper.lundqvist@gmail.com>
2020-02-18 19:04:21 +0100

    Small css improvements

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,30 +1,29 @@
 const btnEls = document.querySelectorAll('.btn');
 
 const updateScore = () => {
-    const player = event.target.closest('.player');
-    const playerScoreEl = player.querySelector('.player__score');
-    const playerScore = parseInt(playerScoreEl.textContent);
-    const modifier = event.target.dataset.key;
-    let result;
-    
-    switch (modifier) {
-        case "+1":
-            result = playerScore + 1;
-            break;
-        case "+10":
-            result = playerScore + 10;
-            break;
-        case "-1":
-            result = playerScore - 1;
-            break;
-        case "-10":
-            result = playerScore - 10;
-            break;
-    }
-    playerScoreEl.textContent = result;
-} 
+  const player = event.target.closest('.player');
+  const playerScoreEl = player.querySelector('.player__score');
+  const playerScore = parseInt(playerScoreEl.textContent);
+  const modifier = event.target.dataset.key;
+  let result;
+
+  switch (modifier) {
+    case '+1':
+      result = playerScore + 1;
+      break;
+    case '+10':
+      result = playerScore + 10;
+      break;
+    case '-1':
+      result = playerScore - 1;
+      break;
+    case '-10':
+      result = playerScore - 10;
+      break;
+  }
+  playerScoreEl.textContent = result;
+};
 
 btnEls.forEach(btn => {
-    btn.addEventListener('click', updateScore);
+  btn.addEventListener('click', updateScore);
 });
-
